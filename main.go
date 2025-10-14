10382d2aeece310872fc6e236b4ea49441426bd2
Omar Ghandour <omarkhaled9913@gmail.com>
2025-10-14 07:19:47 +0300

    Initialize Go module and add main application file with basic Fiber setup

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New(fiber.Config{
+		AppName: "Test",
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		for i := 0; i < 5; i++ {
+			fmt.Println(i)
+		}
+		x := 10
+		p := &x
+		*p = 20
+		fmt.Println(x)
+		return c.SendString("Hello, World! .")
+	})
+
+	app.Listen(":3001")
+}
